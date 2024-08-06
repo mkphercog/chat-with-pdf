137856d0fc5280ab07c184398c814cd669bdebf6
Marcin Hercog <mkp.hercog@gmail.com>
2024-08-06 20:42:46 +0200

    fix route /api/clerk

--- app/api/clerk/route.ts
+++ app/api/clerk/route.ts
@@ -4,13 +4,18 @@ import deleteFilesWithAccount from "@/actions/deleteFilesWithAccount";
 import { WebhookEvent } from "@clerk/nextjs/server";
 
 export async function POST(request: Request) {
-  const payload: WebhookEvent = await request.json();
-
-  if (!payload.data.id) return;
-
-  if (payload.type === "user.deleted") {
-    deleteFilesWithAccount(payload.data.id);
+  if (request.method !== "POST") {
+    return new Response("Method Not Allowed", { status: 405 });
   }
 
-  return Response.json({ message: "Received" });
+  const payload: WebhookEvent = await request.json();
+
+  if (!payload.data?.id)
+    return new Response("Invalid Payload", { status: 400 });
+
+  if (payload.type === "user.deleted") {
+    await deleteFilesWithAccount(payload.data.id);
+  }
+
+  return new Response(JSON.stringify({ message: "Received" }), { status: 200 });
 }
