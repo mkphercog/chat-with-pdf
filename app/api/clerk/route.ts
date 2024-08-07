a416a4cfa84d75b487a2d11546f3e4dbe4388449
Marcin Hercog <mkp.hercog@gmail.com>
2024-08-07 15:18:10 +0200

    add svix to /api/clerk route

--- app/api/clerk/route.ts
+++ app/api/clerk/route.ts
@@ -2,6 +2,23 @@ export const dynamic = "force-dynamic";
 
 import deleteFilesWithAccount from "@/actions/deleteFilesWithAccount";
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
+import { Webhook } from "svix";
+
+const webhookSecret = process.env.CLERK_WEBHOOK_SECRET || "";
+
+const validateRequest = async (request: Request) => {
+  const payloadString = await request.text();
+  const headerPayload = headers();
+
+  const svixHeaders = {
+    "svix-id": headerPayload.get("svix-id")!,
+    "svix-timestamp": headerPayload.get("svix-timestamp")!,
+    "svix-signature": headerPayload.get("svix-signature")!,
+  };
+  const wh = new Webhook(webhookSecret);
+  return wh.verify(payloadString, svixHeaders) as WebhookEvent;
+};
 
 export async function POST(request: Request) {
   if (request.method !== "POST") {
@@ -9,7 +26,7 @@ export async function POST(request: Request) {
   }
 
   try {
-    const payload: WebhookEvent = await request.json();
+    const payload = await validateRequest(request);
 
     console.log("Received payload:", payload);
 
