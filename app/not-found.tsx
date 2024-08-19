aa1af03c1cd173f23ebc3bc5809dc9a1fcc97ff3
Marcin Hercog <mkp.hercog@gmail.com>
2024-08-19 10:40:38 +0200

    change Not-Found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,18 @@
+import { Button } from "@/components/ui/button";
 import { ROUTES } from "@/routes";
-import { RedirectType, redirect } from "next/navigation";
+import { BanIcon } from "lucide-react";
+import Link from "next/link";
 
 const NotFoundPage = () => {
-  redirect(ROUTES.home.root(), RedirectType.replace);
+  return (
+    <div className="h-screen w-full  flex flex-col justify-center items-center gap-4">
+      <BanIcon className="w-24 h-24 text-indigo-600" />
+      <h2 className="font-bold text-2xl">Page not found</h2>
+      <Button asChild>
+        <Link href={ROUTES.home.root()}>HOME PAGE</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default NotFoundPage;
